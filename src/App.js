639ae266c34e0b3a034922da450236e05d43aa15
yamaseee <yamaseee@gmail.com>
2017-02-09 11:55:41 +0700

    11 Use ES2016 Property Initializer Syntax in ES6 classes

--- src/App.js
+++ src/App.js
@@ -5,28 +5,22 @@ import {TodoForm, TodoList} from './components/todo'
 import {addTodo, generateId} from './lib/todoHelpers'
 
 class App extends Component {
-  constructor(){
-    super()
-    this.state = {
-      todos: [
-        {id: 1, name: 'Learn JSX', isComplete: true},
-        {id: 2, name: 'BUILd app', isComplete: false},
-        {id: 3, name: 'ship it',   isComplete: false}
-      ],
-      currentTodo: ''
-    }
-    this.handleInputChange = this.handleInputChange.bind(this)
-    this.handleSubmit = this.handleSubmit.bind(this)
-    this.handleEmptySubmit = this.handleEmptySubmit.bind(this)
+  state = {
+    todos: [
+      {id: 1, name: 'Learn JSX', isComplete: true},
+      {id: 2, name: 'BUILd app', isComplete: false},
+      {id: 3, name: 'ship it',   isComplete: false}
+    ],
+    currentTodo: ''
   }
 
-  handleEmptySubmit (evt) {
+  handleEmptySubmit = (evt) => {
     evt.preventDefault()
     this.setState({
       errorMessage: 'Please supply a todo name'
     })
   }
-  handleSubmit (evt) {
+  handleSubmit = (evt) => {
     evt.preventDefault()
     const newId = generateId()
     const newTodo = {id: newId, name: this.state.currentTodo, isComplete: false}
@@ -38,7 +32,7 @@ class App extends Component {
     })
   }
 
-  handleInputChange (evt) {
+  handleInputChange = (evt) => {
       this.setState({
         currentTodo: evt.target.value
       })
