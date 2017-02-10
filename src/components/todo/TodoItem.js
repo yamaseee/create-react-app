0d4559e28e423c05fb0519d776b848af39b5c706
yamaseee <yamaseee@gmail.com>
2017-02-10 23:11:38 +0700

    13 Pass Data To Event Handlers with Partial Function Application

--- src/components/todo/TodoItem.js
+++ src/components/todo/TodoItem.js
@@ -1,9 +1,12 @@
 import React from 'react'
+import {partial} from '../../lib/utils'
 
 export const TodoItem = (props) => {
+  const handleToggle = props.handleToggle.bind(null, props.id)
   return (
     <li>
-      <input type="checkbox" defaultChecked={props.isComplete}/> {props.name}
+      <input type="checkbox" onChange={handleToggle}
+         checked={props.isComplete}/> {props.name}
     </li>
   )
 }
