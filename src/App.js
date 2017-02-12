9c2100fed9182dfdc3176365125fa9b09b52054a
yamaseee <yamaseee@gmail.com>
2017-02-12 22:53:21 +0700

    15 Remove Items from a List without Mutations

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import {TodoForm, TodoList} from './components/todo'
-import {addTodo, generateId, findById, toggleTodo, updateTodo} from './lib/todoHelpers'
+import {addTodo, generateId, findById, toggleTodo, updateTodo, removeTodo} from './lib/todoHelpers'
 import {pipe, partial} from './lib/utils'
 
 class App extends Component {
@@ -13,6 +13,12 @@ class App extends Component {
       {id: 3, name: 'ship it',   isComplete: false}
     ],
     currentTodo: ''
+  }
+
+  handleRemove = (id, evt) => {
+    evt.preventDefault()
+    const updatedTodos = removeTodo(this.state.todos, id)
+    this.setState({todos: updatedTodos})
   }
 
   handleToggle = (id) => {
@@ -59,9 +65,9 @@ class App extends Component {
             currentTodo={this.state.currentTodo}
             handleSubmit={submitHandler}/>
         </div>
-        <TodoList handleToggle={this.handleToggle} todos={this.state.todos}/>
-
-
+        <TodoList handleToggle={this.handleToggle}
+          todos={this.state.todos}
+          handleRemove={this.handleRemove}/>
       </div>
     );
   }
