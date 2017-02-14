43ce156f9dbe7f902ccfb2d0ce01b11a6eff8f5d
yamaseee <yamaseee@gmail.com>
2017-02-14 17:31:38 +0700

    18 Filter Data on Property Values

--- src/lib/todoHelpers.test.js
+++ src/lib/todoHelpers.test.js
@@ -1,4 +1,4 @@
-import {addTodo, findById, toggleTodo, updateTodo, removeTodo} from './todoHelpers'
+import {addTodo, findById, toggleTodo, updateTodo, removeTodo, filterTodos} from './todoHelpers'
 
 test('addTodo should add the passed todo to the list', () => {
   const startTodos = [
@@ -126,4 +126,61 @@ test('removeTodo should not mutate the original array', () => {
   const result = removeTodo(startTodos, targetId)
 
   expect(result).not.toBe(startTodos)
+})
+
+test('filterTodos should return all items for root route', () => {
+  const startTodos = [
+    {id:1, name: 'one', isComplete: false},
+    {id:2, name: 'two', isComplete: true},
+    {id:3, name: 'three', isComplete: false}
+  ]
+
+  const result = filterTodos(startTodos, '/')
+
+  expect(result).toEqual(startTodos)
+})
+
+test('filterTodos should return all items for root route', () => {
+  const startTodos = [
+    {id:1, name: 'one', isComplete: false},
+    {id:2, name: 'two', isComplete: true},
+    {id:3, name: 'three', isComplete: false}
+  ]
+
+  const result = filterTodos(startTodos, '/')
+
+  expect(result).toEqual(startTodos)
+})
+
+test('filterTodos should return only completed items for the completed route', () => {
+  const startTodos = [
+    {id:1, name: 'one', isComplete: false},
+    {id:2, name: 'two', isComplete: true},
+    {id:3, name: 'three', isComplete: false}
+  ]
+
+  const expected = [
+    {id:2, name: 'two', isComplete: true}
+  ]
+
+  const result = filterTodos(startTodos, '/completed')
+
+  expect(result).toEqual(expected)
+})
+
+test('filterTodos should return only incompleted items for the active route', () => {
+  const startTodos = [
+    {id:1, name: 'one', isComplete: false},
+    {id:2, name: 'two', isComplete: true},
+    {id:3, name: 'three', isComplete: false}
+  ]
+
+  const expected = [
+    {id:1, name: 'one', isComplete: false},
+    {id:3, name: 'three', isComplete: false}
+  ]
+
+  const result = filterTodos(startTodos, '/active')
+
+  expect(result).toEqual(expected)
 })
